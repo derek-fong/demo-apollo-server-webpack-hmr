f1134be9b5fa67ab3795fbe21e566e354dc7d1d5
Derek Fong <derek.fong@alppix.com>
2019-11-03 17:13:13 +0800

    build(npm): install dotenv

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { ApolloServer } from 'apollo-server';
 
+import { environment } from './environment';
 import resolvers from './resolvers';
 import typeDefs from './type-defs';
 
-const server = new ApolloServer({ resolvers, typeDefs });
+const server = new ApolloServer({
+  resolvers,
+  typeDefs,
+  introspection: environment.apollo.introspection,
+  playground: environment.apollo.playground,
+});
 
-server.listen().then(({ url }) => console.log(`Server ready at ${url}. `));
+server
+  .listen(environment.port)
+  .then(({ url }) => console.log(`Server ready at ${url}. `));
 
 if (module.hot) {
   module.hot.accept();
